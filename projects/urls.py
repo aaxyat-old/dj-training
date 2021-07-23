3df3c88a1c0caf309a593d2bbfaffd3014d99d83
aaxyat <aaxyat@gmail.com>
2021-07-23 15:02:21 +0545

    purge Everything

--- projects/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("",views.index, name="index"),
-    path("project/<str:pk>", views.project, name="project"),
-    path("test/", views.test, name="test")
-]
